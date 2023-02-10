3ce40325e58855846892d47fe08e7512315f23a0
ipshitaa <114577348+ipshitaa@users.noreply.github.com>
2023-02-10 10:49:45 +0530

    Add files via upload

---
+++ html programs/dom/index.js
@@ -0,0 +1,12 @@
+alert("hello");
+<script type="text/javascript">
+const a=7;
+let result;
+if(a>0){
+  result="positive";
+}
+else{
+  result="not positive";
+}
+document.writeln(result);
+</script>
